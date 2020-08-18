e7ac63b4f5b920cd701d46ffd9f5277d3013b673
LinaLin123 <jlina.lin@gmail.com>
2020-08-18 15:31:09 +0200

    a lot of updates

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import MyButton from "./components/MyButton";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Vad blir det till middag?</p>
+        <h1> Sushi!</h1>
+
+        <MyButton title="K" />
+        <MyButton title="Hå" />
+        <MyButton title="Hej" />
+        <MyButton title="red"/>
+
+        <h2>
+          <a href="https://alltommat.se/recept/sushi/"> Kika in recepten</a>
+        </h2>
+
+        <img src="./mat.jpeg" alt="food" width="300" height="400" />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
